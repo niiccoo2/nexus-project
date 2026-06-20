a5775f07904d71660a036e8f5483c2084f844373
RichardD242 <richard.daubner@icloud.com>
2026-06-20 06:18:29 +0200

    added simple ^platform and syk

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,25 +1,19 @@
-<svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet" />
-</svelte:head>
+<script>
+    import { onMount } from 'svelte';
+    import { startGame } from '../../game/Game.js';
 
-<div class="page">
-	<h1>Game</h1>
-</div>
+    /** @type {HTMLCanvasElement} */
+    let canvas;
+
+    onMount(() => {
+        startGame(canvas);
+    });
+</script>
+
+<canvas bind:this={canvas}></canvas>
 
 <style>
-	.page {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100vh;
-		font-family: 'Libre Baskerville', serif;
-		background: #fff;
-	}
-
-	h1 {
-		font-size: 3rem;
-		font-weight: 400;
-	}
+    canvas {
+        display: block;
+    }
 </style>
